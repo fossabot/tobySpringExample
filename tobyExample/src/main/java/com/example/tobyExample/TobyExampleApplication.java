22e38c91f6d4efa7887c8136e120fcd4159f1ace
kakao <kakao@kakaoui-MacBookPro.local>
2020-01-01 16:27:13 +0900

    merging with example
    - to solve importing issue(no gradle)
    - new spring project + current folder

---
+++ tobyExample/src/main/java/com/example/tobyExample/TobyExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.tobyExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobyExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TobyExampleApplication.class, args);
+	}
+
+}
